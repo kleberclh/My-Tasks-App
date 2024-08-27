4a3e6778dad46b12bc5992c41b249b8ef1fc9c40
Kleber Coelho <klebercoelhogt@gmail.com>
2024-08-27 18:35:53 -0300

    ajuste no tamanho das tasks

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -8,7 +8,7 @@ export default function TaskList() {
     <div className="bg-white rounded-lg shadow-md mt-10">
       <div className="p-6">
         <h2 className="text-2xl font-bold mb-4">Tasks</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="flex flex-col gap-4 ">
           {tasks.map((task) => (
             <div
               key={task.id}
