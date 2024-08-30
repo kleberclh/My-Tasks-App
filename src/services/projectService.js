8c17dcb35954982ca96edc8e0deaacba7062f638
Kleber Coelho <klebercoelhogt@gmail.com>
2024-08-30 18:54:22 -0300

    #

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:5000";
-const API_URL = "https://www.api.waxystore.shop/";
+const API_URL = "https://www.api.waxystore.shop";
 
 const getUserId = () => {
   const userId = localStorage.getItem("userId");
